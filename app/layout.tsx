539d8a3c38ae90d19f969d83c89d4d447a2568bc
Seggar-zackaria <zakari0002@gmail.com>
2024-12-22 15:08:42 +0100

    mixed updates metaData && login fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import "@/app/ui/global.css";
+import { Metadata } from "next";
 
 import { inter } from "@/app/ui/fonts";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "acme Dashboard",
+  },
+  description: "the official Next.js Course Dashboard, built with ap Router",
+  metadataBase: new URL("htpps://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
